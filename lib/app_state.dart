c3d7a3cfe60ee7b1a5403c7fad6ba41cedba134f
Johan Lantz <yc00025@johan-macbook.local>
2019-07-12 20:15:01 +0200

    comment

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/widgets.dart';
 import 'page_builder.dart';
 
+// This class keeps track of which dynamic page to show
 class AppState with ChangeNotifier {
   Widget dynamicPage = PageBuilder.getPage('page1');
 
   changeDynamicPage() {
+    // Here we replace the original widget with another one. The problem is that 
+    // while the object is replaced, the InputState stays the same (the one from the initial Widget for page1)
     this.dynamicPage = PageBuilder.getPage('page2');
     notifyListeners();
   }
